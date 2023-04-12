15acfa1861959d56f668f310f3ca7b1cf5a56d60
DavidPB7 <davidpereira1701@gmail.com>
2023-04-12 16:19:48 -0200

    a
    a

--- Array_dinamico.java
+++ Array_dinamico.java
@@ -1,6 +1,5 @@
-
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 
 
 public class Array_dinamico {
@@ -25,61 +24,32 @@ public class Array_dinamico {
         }
         System.out.println("-------------------");
 
-        Integer[] novo_numeros = numeros.toArray(new Integer[0]);
-        System.out.println("Elementos do array em ordem crescente: ");
-        Arrays.sort(novo_numeros);
-        System.out.println(Arrays.toString(novo_numeros));
+        Collections.sort(numeros);
+        System.out.println("Elementos do array em ordem crescente: " + numeros);
+        for (int i = 0; i < numeros.size() ; i++) {
+            System.out.println(numeros.get(i));
+        }
+        
+
         System.out.println("-------------------");
 
-        int posicao = Arrays.binarySearch(novo_numeros, 9);
-        System.out.println("Considerando o meu novo array que esta ordenado.");
-        System.out.println("O elemento 9 esta na posicao "+ posicao);
+        System.out.println("Elementos do array maiores:");
+        ArrayList<Integer> maiores = new ArrayList<>(numeros.subList(2, 5));
+
+        for(int j = 0; j < maiores.size(); j++) {
+            System.out.println(maiores.get(j));
+        }
         System.out.println("-------------------");
 
-        Integer[] maiores = new Integer[3];
-        //ArrayList<Integer> maiores = new ArrayList<Integer>();
-        maiores = Arrays.copyOfRange(novo_numeros, 2, 5);
-        System.out.println("Elementos no array maiores: ");
-        System.out.println(Arrays.toString(maiores));
+        boolean resposta = numeros.equals(maiores);
+        System.out.println("Os arrays maiores e numeros sao iguais em elementos e em posicao? " + resposta);
         System.out.println("-------------------");
 
-        //boolean resposta = Arrays.equals(numeros, maiores);
-        //System.out.println("Os arrays 'numeros' e 'maiores apresenta a mesma quantidade de elementos nas mesmas posições:" + resposta);
+        System.out.println("Mostrando os elementos do array numeros em ordem decrescente: ");
+
+        for (int i = numeros.size() - 1; i >= 0; i--) {
+            System.out.println(numeros.get(i));
+        }
 
     }
 }
-
-// import java.util.ArrayList;
-
-
-// public class Array_dinamico {
-//     public static void main(String[] args) {
-//         ArrayList<Integer> numeros = new ArrayList<Integer>();
-//         System.out.println("Olá, Mundo!");
-//         System.out.println("1!");
-
-//         numeros.add(5);
-//         numeros.add(2);
-//         numeros.add(9);
-//         numeros.add(7);
-//         numeros.add(1);
-
-//         for (int i = 0; i < numeros.length; i++);
-//     }
-
-//     static void array_dinamico() {
-//         ArrayList<Integer> numeros = new ArrayList<Integer>();
-//         System.out.println("Olá, Mundo!");
-//         System.out.println("1!");
-
-//         numeros.add(5);
-//         numeros.add(2);
-//         numeros.add(9);
-//         numeros.add(7);
-//         numeros.add(1);
-//     }
-
-//     static void mostrar_array(array) {
-//         for (int i = 0; i < array.length; i++);
-//     }
-// }
